d45b0d7994bc8b4760086f7980ef392dd18580d5
Sean Murdock <scmurdock@gmail.com>
2022-06-09 06:50:20 -0600

    Add comments to script

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 cd /opt/banking-simulation
 # Then pull the latest
 git pull
-# Then run the jar
+# Then run the latest jar
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -Dconfig=/etc/banking-simulation/application.conf -jar /opt/banking-simulation/BankingSimulation-1.0-SNAPSHOT.jar
