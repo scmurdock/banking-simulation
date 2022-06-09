d73e2aba29d92252519970752457228220ab9aff
Sean Murdock <scmurdock@gmail.com>
2022-06-09 06:36:20 -0600

    Add comments

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+# First change to the directory where your repository was cloned to begin with
 cd /opt/banking-simulation
+# Then pull the latest
 git pull
+# Then run the jar
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -Dconfig=/etc/banking-simulation/application.conf -jar /opt/banking-simulation/BankingSimulation-1.0-SNAPSHOT.jar
