3221a38be3dfce49d6bb6abcb66fd171a267fd80
Sean Murdock <scmurdock@gmail.com>
2022-06-09 06:23:30 -0600

    Changed script to git pull instead of git clone

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+cd /opt/banking-simulation
+git pull
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -Dconfig=/etc/banking-simulation/application.conf -jar /opt/banking-simulation/BankingSimulation-1.0-SNAPSHOT.jar
